fe28e3879cdb50ae01288dd529a3ac4804635f7e
lt20190529 <liuxingao1986@126.com>
2019-07-17 13:26:15 +0800

    信息系想

--- src/main/java/com/thinkgem/jeesite/modules/crm/system/flex/web/FlexController.java
+++ src/main/java/com/thinkgem/jeesite/modules/crm/system/flex/web/FlexController.java
@@ -28,7 +28,7 @@ public class FlexController extends BaseController {
 	@Autowired
 	private FlexService flexService;
      
-	//构建项目
+	//构建项目1111
 	@RequestMapping(method=RequestMethod.GET)
 	public String list(@ModelAttribute("flexSet") FlexSet flexSet,
 			@RequestParam(value = "page", defaultValue = "1") int page,
