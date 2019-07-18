53d2cfaf243c34837229959304cf41ab8543cf66
lt20190529 <liuxingao1986@126.com>
2019-07-18 08:53:02 +0800

    20190718

--- src/main/java/com/thinkgem/jeesite/modules/crm/system/flex/web/FlexController.java
+++ src/main/java/com/thinkgem/jeesite/modules/crm/system/flex/web/FlexController.java
@@ -162,7 +162,7 @@ public class FlexController extends BaseController {
 	}
 	
 	/*
-	 * 字典状态修改(Git嘻嘻嘻)
+	 * 字典状态修改
 	 */
 	@RequestMapping(value="toggleStatus/{setId}")
 	public ModelAndView flextoggleStatus(@PathVariable("setId")int flexSetId){
@@ -175,7 +175,7 @@ public class FlexController extends BaseController {
 	}
 	
 	/*
-	 * 删除字典分类(Git) 
+	 * 删除字典分类
 	 */
 	@RequestMapping(value="delete/{setId}")
 	public String flexSetDelete(@PathVariable("setId") int flexSetId) {
