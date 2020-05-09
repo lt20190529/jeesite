c2046e432de45421280b7dcee41e9ccc488bd1a8
lt20190529 <liuxingao1986@126.com>
2020-05-09 17:45:16 +0800

    20200509

---
+++ src/test/java/com/thinkgem/jeesite/spring/springTest.java
@@ -0,0 +1,32 @@
+package com.thinkgem.jeesite.spring;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+public class springTest {
+
+    @Test
+    public void test(){
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource res = resolver.getResource("classpath:test.xml");
+        BeanFactory factory = new XmlBeanFactory(res);
+        Car car = factory.getBean("car1",Car.class);
+        System.out.println("car对象已经初始化完成");
+        System.out.println(car.getMaxSpeed());
+    }
+    @Test
+    public void test1(){
+        ApplicationContext ac=new ClassPathXmlApplicationContext("test.xml");
+        Car car = ac.getBean("car1",Car.class);
+        System.out.println("car对象已经初始化完成111");
+        System.out.println(car.getMaxSpeed());
+
+    }
+}
