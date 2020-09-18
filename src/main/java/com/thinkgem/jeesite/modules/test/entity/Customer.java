251d07872a4b0419a77f7c5c268accfcf7e876f1
lt <541924984@qq.com>
2020-09-18 17:55:17 +0800

    2020091803

---
+++ src/main/java/com/thinkgem/jeesite/modules/test/entity/Customer.java
@@ -0,0 +1,61 @@
+package com.thinkgem.jeesite.modules.test.entity;
+
+public class Customer {
+
+    private int id;
+    private String username;
+    private String address;
+    private String jobs;
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(String jobs) {
+        this.jobs = jobs;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "customer{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", address='" + address + '\'' +
+                ", jobs='" + jobs + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
